ff1af4fa2c243cbe1ee99c9a6b5aa9c8b45e7783
Ryusei-desu <ciru22041@g.nihon-u.ac.jp>
2024-12-25 03:04:20 +0900

    a

--- Assets/Script/ItemEvent.cs
+++ Assets/Script/ItemEvent.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class ItemEvent : MonoBehaviour
@@ -8,20 +7,37 @@ public class ItemEvent : MonoBehaviour
     public GameObject Inventory;
     public GameObject ItemObject;
 
-    private void OnTriggerStay2D(Collider2D other)
+    private bool isPlayerInRange = false;
+
+    private void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.Space))
+        {
+            Item giveItem = ItemObject.GetComponent<Item>();
+
+            int id = giveItem.ItemData.Id;
+            int num = 1;
+
+            Inventory playerInventory = Inventory.GetComponent<Inventory>();
+
+            playerInventory.AddItem(id, num);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            isPlayerInRange = true;
             EventMark.SetActive(true);
-            
         }
-        
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            isPlayerInRange = false;
             EventMark.SetActive(false);
         }
     }
