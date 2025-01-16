2069ac058e25554ebf227ccf78d2cf891e93c081
cimi22132 <cimi22132@g.nihon-u.ac.jp>
2025-01-16 23:22:02 +0900

    titile

--- Assets/Script/title_bottun.cs
+++ Assets/Script/title_bottun.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
+using UnityEngine.UI; // UIの操作にはこの名前空間が必要です
 
-public class NewBehaviourScript : MonoBehaviour
+public class TitleScreen : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        // ボタンオブジェクトを取得
+        Button startButton = GameObject.Find("start").GetComponent<Button>();
+        Button setumeiButton = GameObject.Find("setumei").GetComponent<Button>();
+
+        // ボタンにクリックイベントを追加
+        startButton.onClick.AddListener(OnStartButton);
+        setumeiButton.onClick.AddListener(OnInstructionsButton);
     }
 
     // Update is called once per frame
@@ -15,8 +23,16 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
-    public void testclick()
+
+    // 開始ボタンが押されたときに呼び出される関数
+    public void OnStartButton()
     {
-        Debug.Log("クリックされました！");
+        SceneManager.LoadScene("Riki_Scene");
+    }
+
+    // 説明ボタンが押されたときに呼び出される関数
+    public void OnInstructionsButton()
+    {
+        
     }
 }
