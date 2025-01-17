3b1a7e67fc6c4512a47e30ece0606b75b8efa039
cimi22132 <cimi22132@g.nihon-u.ac.jp>
2025-01-18 00:50:02 +0900

    title

---
+++ Assets/Script/end_bottun.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // UIの操作にはこの名前空間が必要です
+
+public class end_bottun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ボタンオブジェクトを取得
+        Button startButton = GameObject.Find("start").GetComponent<Button>();
+        Button backButton = GameObject.Find("back").GetComponent<Button>(); // backボタンのオブジェクトを取得
+
+        // ボタンにクリックイベントを追加
+        startButton.onClick.AddListener(OnStartButton);
+        backButton.onClick.AddListener(OnBackButton); // backボタンにクリックイベントを追加
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 開始ボタンが押されたときに呼び出される関数
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene("Riki_Scene");
+    }
+
+    // バックボタンが押されたときに呼び出される関数
+    public void OnBackButton()
+    {
+        SceneManager.LoadScene("title_main"); // タイトル画面に戻る
+    }
+}
